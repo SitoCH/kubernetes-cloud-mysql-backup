c596d285d154b166534ca548f75fc958e581e349
Simone Grignola <sito@grignola.ch>
2021-01-03 14:45:35 +0100

    Backup all user databases

--- resources/perform-backup.sh
+++ resources/perform-backup.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 
 # Set the has_failed variable to false. This will change if any of the subsequent database backups/uploads fail.
 has_failed=false
@@ -26,67 +26,76 @@ else
 fi
 
 # Loop through all the databases
-for CURRENT_DATABASE in $(mysql -u $TARGET_DATABASE_USER -p$TARGET_DATABASE_PASSWORD -h $TARGET_DATABASE_HOST -P $TARGET_DATABASE_PORT -e "SELECT schema_name from INFORMATION_SCHEMA.SCHEMATA WHERE schema_name NOT IN('information_schema', 'sys', 'mysql', 'performance_schema')" -s --skip-column-names); do
+for CURRENT_DATABASE in $(mysql -u $TARGET_DATABASE_USER -p$TARGET_DATABASE_PASSWORD -h $TARGET_DATABASE_HOST -P $TARGET_DATABASE_PORT -e 'SHOW DATABASES;' -s --skip-column-names); do
 
-    DUMP=$CURRENT_DATABASE$(date +$BACKUP_TIMESTAMP).sql
-    # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
-    if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $BACKUP_CREATE_DATABASE_STATEMENT $CURRENT_DATABASE 2>&1 >/tmp/$DUMP); then
+    if [ $CURRENT_DATABASE = "information_schema" ] || [ $CURRENT_DATABASE = "sys" ] || [ $CURRENT_DATABASE = "mysql" ] || [ $CURRENT_DATABASE = "performance_schema" ]
+    then
 
-        echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
-
-        # Convert BACKUP_COMPRESS to lowercase before executing if statement
-        BACKUP_COMPRESS=$(echo "$BACKUP_COMPRESS" | awk '{print tolower($0)}')
-
-        # If the Backup Compress is true, then compress the file for .gz format
-        if [ "$BACKUP_COMPRESS" = "true" ]; then
-            gzip -9 -c /tmp/"$DUMP" >/tmp/"$DUMP".gz
-            DUMP="$DUMP".gz
-        fi
-
-        # Optionally encrypt the backup
-        if [ -n "$AGE_PUBLIC_KEY" ]; then
-            cat /tmp/"$DUMP" | age -a -r "$AGE_PUBLIC_KEY" >/tmp/"$DUMP".age
-            echo -e "Encrypted backup with age"
-            DUMP="$DUMP".age
-        fi
-
-        # Convert BACKUP_PROVIDER to lowercase before executing if statement
-        BACKUP_PROVIDER=$(echo "$BACKUP_PROVIDER" | awk '{print tolower($0)}')
-
-        # If the Backup Provider is AWS, then upload to S3
-        if [ "$BACKUP_PROVIDER" = "aws" ]; then
-
-            # If the AWS_S3_ENDPOINT variable isn't empty, then populate the --endpoint-url parameter to use a custom S3 compatable endpoint
-            if [ ! -z "$AWS_S3_ENDPOINT" ]; then
-                ENDPOINT="--endpoint-url=$AWS_S3_ENDPOINT"
-            fi
-
-            # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
-            if awsoutput=$(aws $ENDPOINT s3 cp /tmp/$DUMP s3://$AWS_BUCKET_NAME$AWS_BUCKET_BACKUP_PATH/$DUMP 2>&1); then
-                echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
-            else
-                echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/kubernetes-cloud-mysql-backup.log
-                has_failed=true
-            fi
-
-        fi
-
-        # If the Backup Provider is GCP, then upload to GCS
-        if [ "$BACKUP_PROVIDER" = "gcp" ]; then
-
-            # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
-            if gcpoutput=$(gsutil cp /tmp/$DUMP gs://$GCP_BUCKET_NAME$GCP_BUCKET_BACKUP_PATH/$DUMP 2>&1); then
-                echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
-            else
-                echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $gcpoutput" | tee -a /tmp/kubernetes-cloud-mysql-backup.log
-                has_failed=true
-            fi
-
-        fi
+      echo -e "Database backup skipped for $CURRENT_DATABASE."
 
     else
-        echo -e "Database backup FAILED for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/kubernetes-cloud-mysql-backup.log
-        has_failed=true
+
+      DUMP=$CURRENT_DATABASE$(date +$BACKUP_TIMESTAMP).sql
+      # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
+      if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $BACKUP_CREATE_DATABASE_STATEMENT $CURRENT_DATABASE 2>&1 >/tmp/$DUMP); then
+
+          echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+
+          # Convert BACKUP_COMPRESS to lowercase before executing if statement
+          BACKUP_COMPRESS=$(echo "$BACKUP_COMPRESS" | awk '{print tolower($0)}')
+
+          # If the Backup Compress is true, then compress the file for .gz format
+          if [ "$BACKUP_COMPRESS" = "true" ]; then
+              gzip -9 -c /tmp/"$DUMP" >/tmp/"$DUMP".gz
+              DUMP="$DUMP".gz
+          fi
+
+          # Optionally encrypt the backup
+          if [ -n "$AGE_PUBLIC_KEY" ]; then
+              cat /tmp/"$DUMP" | age -a -r "$AGE_PUBLIC_KEY" >/tmp/"$DUMP".age
+              echo -e "Encrypted backup with age"
+              DUMP="$DUMP".age
+          fi
+
+          # Convert BACKUP_PROVIDER to lowercase before executing if statement
+          BACKUP_PROVIDER=$(echo "$BACKUP_PROVIDER" | awk '{print tolower($0)}')
+
+          # If the Backup Provider is AWS, then upload to S3
+          if [ "$BACKUP_PROVIDER" = "aws" ]; then
+
+              # If the AWS_S3_ENDPOINT variable isn't empty, then populate the --endpoint-url parameter to use a custom S3 compatable endpoint
+              if [ ! -z "$AWS_S3_ENDPOINT" ]; then
+                  ENDPOINT="--endpoint-url=$AWS_S3_ENDPOINT"
+              fi
+
+              # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
+              if awsoutput=$(aws $ENDPOINT s3 cp /tmp/$DUMP s3://$AWS_BUCKET_NAME$AWS_BUCKET_BACKUP_PATH/$DUMP 2>&1); then
+                  echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+              else
+                  echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/kubernetes-cloud-mysql-backup.log
+                  has_failed=true
+              fi
+
+          fi
+
+          # If the Backup Provider is GCP, then upload to GCS
+          if [ "$BACKUP_PROVIDER" = "gcp" ]; then
+
+              # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
+              if gcpoutput=$(gsutil cp /tmp/$DUMP gs://$GCP_BUCKET_NAME$GCP_BUCKET_BACKUP_PATH/$DUMP 2>&1); then
+                  echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+              else
+                  echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $gcpoutput" | tee -a /tmp/kubernetes-cloud-mysql-backup.log
+                  has_failed=true
+              fi
+
+          fi
+
+      else
+          echo -e "Database backup FAILED for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/kubernetes-cloud-mysql-backup.log
+          has_failed=true
+      fi
+
     fi
 
 done
